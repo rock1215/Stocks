e141e0f6e8833e604057d1499a4f8334000cb61e
Yansong Wang <rock1215@outlook.com>
2022-05-06 03:41:44 +0800

    Initial Commit

---
+++ StockList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockList
+//
+//  Created by Yansong Wang on 2022/5/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
